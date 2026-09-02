49323973ed6559d1efb0d0f0842dc7a27e1a260d
windy003 <928920471@qq.com>
2026-09-02 17:05:45 +0800

    调整

--- Sync_To_Local_Or_SMB.cmd
+++ Sync_To_Local_Or_SMB.cmd
@@ -1,1 +1,1 @@
-start /b pythonw "D:\files\using\Python\P_PC_Sync_To_Another_Local_Path_Or_SMB\Sync_To_Local_Or_SMB.py"
+start /b ""  "D:\files\using\Python\P_PC_Sync_To_Another_Local_Path_Or_SMB\.venv\Scripts\pythonw.exe" "D:\files\using\Python\P_PC_Sync_To_Another_Local_Path_Or_SMB\Sync_To_Local_Or_SMB.py"
